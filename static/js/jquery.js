ef9962b15d4521909402a61ba98d13320a96f1b0
Ubuntu <ubuntu@ip-172-31-45-221.ec2.internal>
2019-11-10 17:32:34 +0000

    First comment

---
+++ static/js/jquery.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.collapsible').collapsible();
+    $('select').material_select();
+});
+
+// Show sideNav
+$('.button-collapse').sideNav('show');
+// Hide sideNav
+$('.button-collapse').sideNav('hide');
+// Destroy sideNav
+$('.button-collapse').sideNav('destroy');
+$(".button-collapse").sideNav();
