70bb97d58e069b256b86c75341b164c27906260b
Ubuntu <ubuntu@ip-172-31-45-221.ec2.internal>
2019-11-10 17:29:14 +0000

    Beautify the code

--- static/js/min.js
+++ static/js/min.js
@@ -1,15 +1,14 @@
 // this for password confirm Sign Up form
 var password = document.getElementById("password"),
-confirm_password = document.getElementById("confirm_password");
-  
+    confirm_password = document.getElementById("confirm_password");
+
 // This message appears when the password does not match
-function validatePassword(){
-  if(password.value != confirm_password.value) {
-    confirm_password.setCustomValidity("Passwords Don't Match");
-  }
-  else {
-    confirm_password.setCustomValidity('');
-  }
+function validatePassword() {
+    if (password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Passwords Don't Match");
+    } else {
+        confirm_password.setCustomValidity('');
+    }
 }
 
 password.onchange = validatePassword;
