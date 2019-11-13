f973b9e8e23cc023253ec4403ad787c01aa13bb2
Ubuntu <ubuntu@ip-172-31-45-221.ec2.internal>
2019-11-13 21:25:39 +0000

    Categories

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ files = GridFS(mongo.db)
 @app.route('/home')
 def home():
     return render_template("home.html", login=mongo.db.IQ2019.find())
+
     
 # for register form to To save data in mongodb
 @app.route('/register', methods=['POST']) 
@@ -27,6 +28,7 @@ def register():
     password = data['password']
     mongo.db.users.insert_one({"user_name": user_name, "password": password})
     return redirect(url_for('get_recipes'))
+    
     
 # for login form to join with mongodb data
 @app.route('/login', methods=['POST'])
@@ -68,6 +70,7 @@ def insert_recipe():
     recipes.insert_one(recipe_data)
     return redirect(url_for('get_recipes'))
 
+
 #for get recipes
 @app.route('/get_recipes')
 def get_recipes():
@@ -81,6 +84,7 @@ def search():
     recipe_name = search_data["search"]
     mongo_data = mongo.db.recipes.find({"recipe_name": recipe_name})
     return render_template("recipes.html", recipes=mongo_data)
+ 
     
 # for edit recipe
 @app.route('/edit_recipe/<recipe_id>')
@@ -89,6 +93,7 @@ def edit_recipe(recipe_id):
     all_Categories =  mongo.db.OfIraqMDB.find()
     return render_template('editrecipe.html', recipe=the_recipe,
                            Categories=all_Categories)
+  
     
 #for update recipe                       
 @app.route('/update_recipe/<recipe_id>', methods=["POST"])
@@ -122,6 +127,14 @@ def delete_recipe(recipe_id):
     mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
     return redirect(url_for('get_recipes'))
     
+
+# categories
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html',
+                           categories=mongo.db.OfIraqMDB.find())
+                           
+                           
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
              port=int(os.environ.get('PORT')),
