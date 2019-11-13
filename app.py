8739d1cf80e40a56e894e91f7c3d9c9c6997ebca
Ubuntu <ubuntu@ip-172-31-45-221.ec2.internal>
2019-11-13 21:30:26 +0000

    Add Categories

--- app.py
+++ app.py
@@ -162,6 +162,11 @@ def insert_category():
     mongo.db.OfIraqMDB.insert_one(category_doc)
     return redirect(url_for('get_categories'))
     
+    
+@app.route('/add_category')
+def add_category():
+    return render_template('addcategory.html')
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
              port=int(os.environ.get('PORT')),
