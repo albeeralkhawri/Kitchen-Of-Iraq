96c527995b6231ba21262326e631ba85fe4cdcfc
Ubuntu <ubuntu@ip-172-31-45-221.ec2.internal>
2019-11-13 21:28:42 +0000

    Delete Categories

--- app.py
+++ app.py
@@ -149,6 +149,12 @@ def update_category(category_id):
         {'category_name': request.form.get('category_name')})
     return redirect(url_for('get_categories'))
     
+    
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.OfIraqMDB.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
              port=int(os.environ.get('PORT')),
